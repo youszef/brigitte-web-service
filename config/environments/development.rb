bd2df712d9dc440b5dd5384e2b738497e24c60c2
youszef <zouhari_y@hotmail.com>
2020-11-27 17:35:33 +0100

    fix & undo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,12 +45,12 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.log_level = :info
+  config.log_level = :debug
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
